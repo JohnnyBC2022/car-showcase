3026297a17fbfb42bf941075f7a0e65358c968d5
Johnny <jonathanbc2012@gmail.com>
2024-11-13 19:14:49 +0100

    feat: CustomFilter, SearchBar and SearchManufacturer created

---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+export const footerLinks = [
+    {
+        title: "Acerca de",
+        links: [
+            { title: "Como funciona", url: "/" },
+            { title: "Nosotros", url: "/" },
+            { title: "Socios", url: "/" },
+            { title: "Patrocinadores", url: "/" },
+        ],
+    },
+    {
+        title: "Compañía",
+        links: [
+            { title: "Eventos", url: "/" },
+            { title: "Blog", url: "/" },
+            { title: "Podcast", url: "/" },
+            { title: "Invita a un amigo", url: "/" },
+        ],
+    },
+    {
+        title: "RedesSociales",
+        links: [
+            { title: "Discord", url: "/" },
+            { title: "Instagram", url: "/" },
+            { title: "Twitter", url: "/" },
+            { title: "Facebook", url: "/" },
+        ],
+    },
+];
